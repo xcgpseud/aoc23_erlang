aefd4badc991e05e01a0dd760847f614aa4fb2df
xcgpseud <chris.evans92@hotmail.com>
2024-10-15 18:38:19 +0100

    Oops - missed this file

---
+++ src/days/1/day1.erl
@@ -0,0 +1,16 @@
+-module(day1).
+-export([solve/0]).
+-import(file_helper, [readfile/2]).
+-import(list_helper, [first/2, last/2]).
+-import(type_helper, [is_digit/1]).
+
+solve() ->
+    Lines = file_helper:readfile("src/days/1/day1.txt", fun(Line) ->
+        extract_numbers(binary_to_list(Line))
+    end),
+    io:format("Result: ~p\n", [lists:sum(Lines)]).
+
+extract_numbers(Line) ->
+    First = first(fun(N) -> is_digit(N) end, Line),
+    Last = last(fun(N) -> is_digit(N) end, Line),
+    list_to_integer([First, Last]).
