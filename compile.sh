c6d93924c833e570577f9c45bcb0f03d290b41db
xcgpseud <chris.evans92@hotmail.com>
2024-10-15 18:31:31 +0100

    Update file structure and update the naming of files

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
-erlc "helpers/file_helper.erl"
-erlc "helpers/list_helper.erl"
-erlc "helpers/type_helper.erl"
-erlc "day1/c1.erl"
+rm *.beam
+
+HELPERS="src/helpers"
+DAYS="src/days"
+
+erlc "${HELPERS}/file_helper.erl"
+erlc "${HELPERS}/list_helper.erl"
+erlc "${HELPERS}/type_helper.erl"
+erlc "${DAYS}/1/day1_part1.erl"
